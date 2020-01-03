548b73355de7e9fff37f7fc2c5873dc45d59ceb9
Thomas Humphrey <thumphrey9123@gmail.com>
2020-01-03 02:17:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,6 +120,7 @@ end
 def num_points_scored(name)
   game_hash.each do|place, team| 
     team.each do|attribute, data| 
+      binding.pry
       if attribute == :players
       data.each do |player|
           if player[:player_name] == name
